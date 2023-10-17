04fff8982fe093cade1f2eca21110be7617c3724
yaroslav <yaroslav_filippov_2017@mail.ru>
2023-10-17 20:33:06 +0200

    fix: db connection

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"go.uber.org/zap"
 	"urban-map/internal"
 	"urban-map/internal/utils/db"
 )
@@ -10,6 +9,5 @@ func main() {
 	internal.Init()
 	d := db.Connect()
 	defer db.Close(d)
-	zap.L().Info("DB connection wasn't close: ")
 
 }
